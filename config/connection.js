6a93830c015f22ee69d32927f3d4b24c678fd9b6
gabel35 <gabrieljose3135@gmail.com>
2021-02-13 13:48:14 -0500

    created orm

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
   password: process.env.PASSWORD,
   database: 'burgers_db',
 });
