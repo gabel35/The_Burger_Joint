dedd16982c2192d034ca291c48b336128476771a
gabel35 <gabrieljose3135@gmail.com>
2021-02-13 13:58:27 -0500

    started sql schemas

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	sleepy BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Molly');
+INSERT INTO burgers (name) VALUES ('Charlie');
+INSERT INTO burgers (name, sleepy) VALUES ('Poppy', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Oscar', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Smudge', true);
+INSERT INTO burgers (name) VALUES ('Daisy');
